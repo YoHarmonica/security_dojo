d0d0854a137aaf509caebde267ea26f9b567ccf4
shimura <ms16325@athome.co.jp>
2017-02-23 17:01:18 +0900

    0223勉強会

--- app/controllers/trials_controller.rb
+++ app/controllers/trials_controller.rb
@@ -8,7 +8,15 @@ class TrialsController < ApplicationController
   end
 
   def create
-    @trial = Trial.new examination_id: params[:examination_id], user_id: current_user.id
+    @trial = Trial.new(trial_params)
+    @trial.user_id = current_user.id
     @trial.save
   end
+
+  private
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def trial_params
+      params.require(:trial).permit(results_attributes: [ :answer ])
+    end
 end
